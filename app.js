214dd03393d811644be80bfa9b868d41125151b3
rodritazzi <60492004+rodritazzi@users.noreply.github.com>
2020-05-14 21:31:18 -0300

    sdf

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ fetch('http://quotes.stormconsultancy.co.uk/random.json')
     .then(function(response) {
         return response.json();
     })
-    .then(function(responsee) {
-        console.log(responsee);
+    .then(function(response) {
+        console.log(response);
+        console.log(response.author)
+        document.getElementById("quote").innerText = response.quote;
+        document.getElementById("autor").innerText = response.author;
+        
     });
